a65037bcd1615dc2b3d6f0706d0159d893440483
Lletfrix <lletfrix@gmail.com>
2018-02-09 16:50:13 +0100

    Ejercicio 8 control de errores

--- ejercicio8.c
+++ ejercicio8.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (){
-    
+#define EXECL 0
+#define EXECLP 1
+#define EXECV 2
+#define EXECVP 3
+
+void usage(){
+    printf("Usage is: ./ejercicio8 <program1> <program2> .... <-l|-lp|-v|-vp>\n");
+}
+
+int main (int argc, char **argv){
+    int exec_choice;
+    if(argc <= 2){
+        usage();
+    }
+
+    if(!strcmp(argv[argc-1], "-l")){
+        exec_choice = EXECL;
+    }else if(!strcmp(argv[argc-1], "-lp"){
+        exec_choice = EXECLP;
+    }else if(!strcmp(argv[argc-1], "-v"){
+        exec_choice = EXECV;
+    }else if(!strcmp(argv[argc-1], "-vp"){
+        exec_choice = EXECVP;
+    }else{
+        printf("Argument number %d is wrong.\n", argc-1);
+        usage();
+    }
 }
