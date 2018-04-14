b8fbf0789ed9970b45b3064d30fffdcda4497355
lletfrix <lletfrix@gmail.com>
2018-04-14 19:28:51 +0200

    Pre-Done

--- 3P/ejercicio5.c
+++ 3P/ejercicio5.c
@@ -6,18 +6,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
 
 #define NUM_PROC 3
 #define MAX_MEM 2048
 #define KEY 1300
 #define PATH "/bin/bash"
 
-void a(char *f_in, pid_t pid);
+void a(char *f_in);
 void b(pid_t pid_aux);
-void c(char *f_out);
+void c(char *f_out, pid_t pid_aux);
 void usage();
 void handler_sigusr1(int sig);
-void handler_sigusr2(int sig);
+/*void handler_sigusr2(int sig);*/
 void shift_letras(char *mensaje);
 
 struct message{
@@ -31,54 +33,69 @@ static int qid;
 
 int main(int argc, char *argv[]) {
     int i;
-    pid_t pid, pid_aux;
+    pid_t pid, pid_aux, pids[NUM_PROC];
+    void handler_sigusr1();
     if(argc != 3){
         usage();
         exit(EXIT_SUCCESS);
     }
-    if(qid = msgget(ftok(PATH, KEY), IPC_CREAT|0600) == -1){
+    if((qid = msgget(ftok(PATH, KEY), IPC_CREAT|0600)) == -1){
         perror("Error al crear la cola de mensajes");
         exit(EXIT_FAILURE);
     }
+    printf("%d\n", qid);
+    signal(SIGUSR1, handler_sigusr1);
     for(i = 0; i < NUM_PROC; ++i){
         if(!(pid = fork())){
             switch (i) {
                 case 0:
-                    c(argv[2]);
+                    printf("Soy A: %d\n", getpid());
+                    a(argv[1]);
                     exit(EXIT_FAILURE);
                 case 1:
+                    printf("Soy B: %d\n", getpid());
                     b(pid_aux);
                     exit(EXIT_FAILURE);
                 case 2:
-                    a(argv[1], pid_aux);
+                    printf("Soy C: %d\n", getpid());
+                    c(argv[2], pid_aux);
                     exit(EXIT_FAILURE);
                 default:
                     exit(EXIT_FAILURE);
             }
         }
         pid_aux = pid;
+        pids[i] = pid;
     }
+    /*for(i = 0; i < NUM_PROC; ++i){
+        kill(pids[i], SIGUSR1);
+    }*/
     while(wait(NULL) != -1);
     msgctl(qid, IPC_RMID, NULL);
     exit(EXIT_SUCCESS);
 }
-void a(char *f_in, pid_t pid){
+void a(char *f_in){
     FILE* fp;
-    struct message mensaje;
+    struct message mensaje = {0};
+    //pause();
     fp = fopen(f_in, "r");
+    //sleep(60);
     if(!fp){
         perror("Error abriendo archivo de lectura");
         exit(EXIT_FAILURE);
     }
     mensaje.mtype = 1;
-    while(fread(mensaje.texto, MAX_MEM, 1, fp)){
-        if(-1 == msgsnd(qid, &mensaje, MAX_MEM/sizeof(char), 0)){
-            perror("Error mandando mensaje");
+    while(!feof(fp)){
+        fread(mensaje.texto, MAX_MEM, 1, fp);
+        if(-1 == msgsnd(qid,(struct msgbuf *) &mensaje, MAX_MEM/sizeof(char), 0)){
+            fprintf(stderr, "Error mandando mensaje en el proceso: %d, msqid: %d, mtype: %ld, mtext\n %s\n Error: %s\n", getpid(), qid, mensaje.mtype, mensaje.texto, strerror(errno));
+            //perror("Error mandando mensaje");
             fclose(fp);
             exit(EXIT_FAILURE);
         }
+        printf("Se supone que he enviado el mensaje\n");
     }
-    kill(pid, SIGUSR1);
+    //kill(pid, SIGUSR1);
     fclose(fp);
     exit(EXIT_SUCCESS);
 }
@@ -87,32 +104,51 @@ void b(pid_t pid_aux){
     short ret;
     /*struct message mensaje_recibido;
     struct message mensaje_enviado;*/
-    struct message mensaje;
-    signal(SIGUSR1, handler_sigusr1);
-    while(a_is_running || (ret = msgrcv(qid, &mensaje, MAX_MEM/sizeof(char), 1, IPC_NOWAIT)) != -1){
-        if(ret == -1){
+    struct message mensaje = {0};
+    //signal(SIGUSR1, handler_sigusr1);
+    //sleep(60);
+    //pause();
+    while((ret = msgrcv(qid,(struct msgbuf *) &mensaje, MAX_MEM/sizeof(char), 1, IPC_NOWAIT))){
+        printf("%d\n", ret);
+        printf("%s\n", mensaje.texto);
+        if(errno == ENOMSG){
+            if(kill(pid_aux, 0)){
+                break;
+            }
+            errno = !ENOMSG;
             continue;
         }
+        printf("El mensaje recibido por B es de tipo: %ld\n", mensaje.mtype);
         mensaje.mtype = 2;
+        //printf("%s\n", mensaje.texto);
         shift_letras(mensaje.texto);
-        if(-1 == msgsnd(qid, &mensaje, MAX_MEM/sizeof(char), 0)){
-            perror("Error mandando mensaje");
-            exit(EXIT_SUCCESS);
+        //printf("%s\n", mensaje.texto);
+        if(-1 == msgsnd(qid,(struct msgbuf *)  &mensaje, MAX_MEM/sizeof(char), 0)){
+            fprintf(stderr, "Error mandando mensaje en el proceso: %d, msqid: %d, mtype: %ld, mtext\n %s\n Error: %s\n", getpid(), qid, mensaje.mtype, mensaje.texto, strerror(errno));
+            //perror("Error mandando mensaje");
+            exit(EXIT_FAILURE);
         }
     }
-    kill(pid_aux, SIGUSR2);
+    //kill(pid_aux, SIGUSR2);
     exit(EXIT_SUCCESS);
 }
 
-void c(char *f_out){
-    struct message mensaje;
-    signal(SIGUSR2, handler_sigusr2);
+void c(char *f_out, pid_t pid_aux){
+    struct message mensaje = {0};
     short ret;
+    //pause();
+    //sleep(60);
+    //signal(SIGUSR2, handler_sigusr2);
     FILE *fp = fopen(f_out, "w");
-    while(b_is_running || (ret = msgrcv(qid, &mensaje, MAX_MEM/sizeof(char), 2, IPC_NOWAIT)) != -1){
-        if(ret == -1){
+    while((ret = msgrcv(qid,(struct msgbuf *) &mensaje, MAX_MEM/sizeof(char), 2, IPC_NOWAIT))){
+        if(errno == ENOMSG){
+            if(kill(pid_aux, 0)){
+                break;
+            }
+            errno = !ENOMSG;
             continue;
         }
+        printf("El mensaje recibido por C es de tipo: %ld\n", mensaje.mtype);
         fprintf(fp, "%s", mensaje.texto);
     }
     fclose(fp);
@@ -124,7 +160,7 @@ void shift_letras(char *mensaje){
         //*mensaje = (*mensaje - 'a' + 1) % ('z' - 'a' + 1) + 'a';
         if(*mensaje >= 'A' && *mensaje < 'Z'){
             *mensaje = (*mensaje - 'A' + 1) % ('Z' - 'A' + 1) + 'A';
-        } else if(*mensaje >= 'a' && *mensaje >= 'z'){
+        } else if(*mensaje >= 'a' && *mensaje <= 'z'){
             *mensaje = (*mensaje - 'a' + 1) % ('z' - 'a' + 1) + 'a';
         }
         mensaje++;
@@ -136,9 +172,9 @@ void usage(){
 }
 
 void handler_sigusr1(int sig){
-    a_is_running = 0;
+    return;
 }
-
+/*
 void handler_sigusr2(int sig){
     b_is_running = 0;
-}
+}*/
