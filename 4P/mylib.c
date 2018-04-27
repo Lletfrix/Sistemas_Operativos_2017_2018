50881b422fc76ed5a36397a967eb55196a280e48
sergamar <sergamar23@hotmail.es>
2018-04-27 17:00:24 +0200

    Initial 4p

---
+++ 4P/mylib.c
@@ -0,0 +1,52 @@
+/**
+ * @brief Funciones personales varias. Implementacion de mylib.h.
+ *
+ * Este fichero contiene el código fuente de las funciones en la interfaz mylib.h.
+ * @file mylib.c
+ * @author Rafael Sánchez & Sergio Galán
+ * @version 1.0
+ * @date 12-04-2018
+ */
+#include <signal.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "mylib.h"
+
+void sigaddset_var(sigset_t *sigt, int sig, ...){
+    va_list args;
+    int i;
+
+    va_start(args, sig);
+    for (i = sig; i != -1; i = va_arg(args, int)) {
+        sigaddset(sigt, i);
+    }
+    va_end(args);
+    return;
+}
+
+float randNum(float inf, float sup){
+    float temp, r;
+    if (!(temp = sup - inf)){
+        return temp;
+    }else if(temp < 0){
+        temp = inf;
+        inf = sup;
+        sup = temp;
+    }
+    r = (float) inf + rand() / (RAND_MAX / (sup - inf) + 1.0);
+    return r;
+}
+
+bool aredigits(const char *string){
+    int i;
+    for(i = 0; i < strlen(string); ++i){
+        if (!isdigit(string[i])){
+            return false;
+        }
+    }
+    return true;
+}
---
+++ 4P/mylib.h
@@ -0,0 +1,46 @@
+/**
+ * @brief Interfaz de funciones personales varias.
+ *
+ * Este fichero contiene las funciones personales varias.
+ *
+ * @file mylib.h
+ * @author Rafael Sánchez & Sergio Galán
+ * @version 1.0
+ * @date 12-04-2018
+ */
+#ifndef MYLIB
+#define MYLIB
+
+/**
+ * @brief Añade una lista de señales a un set.
+ *
+ * Añade una lista de señales acabada en -1 al set dado por sigt.
+ * Ejemplo de uso : sigaddset_var(sigt, SIGUSR1, SIGUSR2, SIGINT, -1);
+ *
+ * @param sigt Puntero de tipo sigset_t. Set de señales.
+ * @param sig Indices de las distinas señales. De extension variable. Debe terminar en -1.
+ */
+void sigaddset_var(sigset_t *sigt, int sig, ...);
+
+/**
+ * @brief Genera un número real aleatorio en el rango [inf, sup)
+ *
+ * Genera un número real aleatorio en el rango [inf, sup)
+ *
+ * @param inf Límite inferior del intervalo
+ * @param sup Límite superior del intervalo
+ * @return Devuelve el número aleatorio generado
+ */
+float randNum(float inf, float sup);
+
+/**
+ * @brief Comprueba si una string es numerica.
+ *
+ * Comprueba si cada caracter de la cadena string es un valor numerico.
+ *
+ * @param string Cadena de caracteres.
+ * @return Devuelve true si la cadena es numerica, false si no lo es.
+ */
+bool aredigits(const char *string);
+
+#endif
